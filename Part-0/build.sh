59452c20725ac5d7512ccb210bb66251b670d5c7
alexEhrenberg <ehrenberg.work@gmail.com>
2019-09-01 13:43:34 -0400

    Create build.sh

---
+++ Part-0/build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# how to build a game server from scratch (using uwebsockets on digitalOcean's servers with a centos image)
+yum update -y
+yum install gcc-c++ -y
+yum install openssl-devel -y
+yum groupinstall "Development Tools" -y
+
+yum install wget -y
+
+wget "https://cmake.org/files/v3.12/cmake-3.12.0.tar.gz"
+tar xvzf cmake-3.12.0.tar.gz
+cd cmake-3.12.0
+./bootstrap && make && make install
+cd ..
+
+wget "https://github.com/libuv/libuv/archive/v1.x.zip"
+unzip v1.x.zip
+cd libuv-1.x
+sh autogen.sh
+./configure
+make
+make check
+make install
+cd ..
+
+yum install boost-devel -y
+
+# download website
+wget "https://github.com/alexEhrenberg/website/archive/master.zip"
+unzip master.zip
+cd website-master
+
+# build uWebSockets
+wget "https://github.com/uNetworking/uWebSockets/archive/v0.14.8.zip"
+unzip v0.14.8.zip
+cd uWebSockets-0.14.8
+make
+make install
+cd ..
+
+# build website
+cmake .
+make
+export LD_LIBRARY_PATH=/usr/local/lib
+./server
